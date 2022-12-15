dd31cc73b4c9f1c69181e632d76141e39f441193
Quan Liu <quan.liu@mpimet.mpg.de>
2022-12-16 04:02:27 +0800

    path

--- scripts/generate_md.py
+++ scripts/generate_md.py
@@ -2,7 +2,7 @@ import src.data_generator as data_generator
 import src.md_generator as md_generator
 
 file_name = "../docs/source/note_example.md"  # change here if the path not right
-plot_dir = "../docs/source/plots/"
+plot_dir = "./plots/"             # change the dir if the path not right
 
 try:
     md_file = md_generator.doc_quick_plots(file_name,title = "example note", plot_dir = plot_dir)
