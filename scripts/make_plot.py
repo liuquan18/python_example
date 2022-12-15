13e63466db8c48623d6b334cd01298bf3aa38544
Quan Liu <quan.liu@mpimet.mpg.de>
2022-12-16 03:51:27 +0800

    dir as var

--- scripts/make_plot.py
+++ scripts/make_plot.py
@@ -2,15 +2,20 @@
 import matplotlib.pyplot as plt
 import src.data_generator as data_generator
 
+plot_dir = "../docs/source/plots/"  # change here if path is not right
+
+
 x,y = data_generator.generatexy()
 
+# plot one
 plt.plot(x,y,'r--')
-plt.suptitle("red line plot")
-plt.savefig("../docs/source/plots/red_line.png")
+plt.suptitle(plot_dir + "red line plot")
+plt.savefig("red_line.png")
 print("red fig saved")
 
+# plot two
 plt.plot(x,y,'g--')
 plt.suptitle("green line plot")
-plt.savefig("../docs/source/plots/green_line.png")
+plt.savefig(plot_dir + "green_line.png")
 print("green fig saved")
 
