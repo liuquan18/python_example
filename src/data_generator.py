96b4e67bc03c8d0a9903dc51f21432fb1a209d9f
Quan Liu <quan.liu@mpimet.mpg.de>
2022-12-16 03:07:33 +0800

    doc for the func

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,5 +1,5 @@
 def generatexy():
-    """generate random series of x and y for plot. 
+    """generate random series of x and y for ploting. 
 
     You could add a more detailed description of the function here
     if it were more complicated than simply adding two arguments.
@@ -10,9 +10,6 @@ def generatexy():
     Returns:
         float: x and y
 
-    Examples:
-        >>> randomxy(1)
-        3
     """
     x = [1,2,3,4,5,6,7,8,9,10]
     radom_letters = 'claraBaley'
