766743f8456ccbbb449572ec526714677bf592b3
Quan Liu <quan.liu@mpimet.mpg.de>
2022-12-16 03:04:23 +0800

    test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,6 @@
-import src.data_generator
+import src.data_generator as data_generator
 
 
 def test_func():
-    x,y = functions.generatexy()
+    x,y = data_generator.generatexy()
     assert x[0]==1
