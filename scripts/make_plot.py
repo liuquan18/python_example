c4cffcbde1a961bd551a261ee56d5683869ed93f
Quan Liu <quan.liu@mpimet.mpg.de>
2022-12-16 03:23:03 +0800

    two plots

--- scripts/make_plot.py
+++ scripts/make_plot.py
@@ -7,5 +7,9 @@ x,y = data_generator.generatexy()
 
 plt.plot(x,y,'r--')
 plt.savefig("../docs/source/plots/red_line.png")
-print("fig saved")
+print("red fig saved")
+
+plt.plot(x,y,'g--')
+plt.savefig("../docs/source/plots/green_line.png")
+print("green fig saved")
 
