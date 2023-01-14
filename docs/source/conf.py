39d8d4c80a24a463f7a633ee88d4731ec7147c77
quan <quan.liu@mpimet.mpg.de>
2023-01-14 16:50:11 +0100

    change name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'html_example'
+project = 'python_example'
 copyright = '2022, mypackage developers'
 author = 'Quan'
 
